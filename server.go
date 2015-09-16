0cd86627eefa90f13337ac8495df8ea1177f8d2b
Ben Osheroff <ben@zendesk.com>
2015-09-16 10:50:26 -0700

    move everything into server.go

--- server.go
+++ server.go
@@ -4,6 +4,10 @@ import (
 	"fmt"
 	"math/rand"
 	"net"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 type Server interface {
@@ -27,4 +31,30 @@ func GetPort(suggestedPort int) int {
 		}
 	}
 	return 0
+}
+
+func pidExists(pid int) bool {
+	proc, err := os.FindProcess(pid)
+	if err != nil {
+		return false
+	}
+	err = proc.Signal(syscall.Signal(0))
+	return err == nil
+}
+
+func WatchServer(ppid int, server Server) {
+	channel := make(chan os.Signal, 1)
+
+	signal.Notify(channel, os.Interrupt, os.Kill)
+	go func() {
+		for true {
+			if !pidExists(ppid) || !pidExists(server.Pid()) {
+				channel <- os.Kill
+			}
+			time.Sleep(1 * time.Second)
+		}
+	}()
+	<-channel
+
+	server.Kill()
 }
